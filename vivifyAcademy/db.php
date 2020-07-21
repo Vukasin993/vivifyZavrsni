a346b4a2e39667a390c73a4a2a41b4ec0f0748d8
Vukasin Macura <moj_email@gmail.com>
2020-07-21 18:53:13 +0200

    Uradjen 4. zadatak

---
+++ vivifyAcademy/db.php
@@ -0,0 +1,16 @@
+<?php 
+        $servername = 'localhost';
+        $username = 'root';
+        $password = 'vivify';
+        $dbname = 'blog';
+    
+        try {
+            $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          }
+          catch(PDOException $e)
+              {
+                  echo $e->getMessage();
+              }
+
+?>
